417bb2c50cc7444aacb562ef363b2815d8a7701d
FINIKKKK <digitallkrash@gmail.com>
2023-08-22 12:39:43 +0600

    post page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,34 +1,31 @@
-import {StatusBar} from 'react-native';
 import React from 'react';
 import Post from '../components/Post';
 import axios from 'axios';
 import styled from "styled-components/native";
-import {FlatList, TouchableOpacity} from "react-native";
+import {FlatList, TouchableOpacity, RefreshControl, StatusBar} from "react-native";
 
 
 export const Home = ({navigation}) => {
     const [posts, setPosts] = React.useState([])
-    React.useEffect(() => {
+    const [isLoading, setIsLoading] = React.useState(false)
+
+    const onFetchPosts = () => {
         axios.get('https://freefakeapi.io/api/posts?limit=15').then(({data}) => {
             setPosts(data)
         })
-    })
+    };
+
+    React.useEffect(() => onFetchPosts())
 
     return (
-        <>
-            <StatusBar theme='auto'/>
-            <FlatList
-                data={posts}
-                renderItem={({item}) => (
-                    <TouchableOpacity onPress={() => navigation.navigate('post')}>
-                        <Post title={item.title} body={item.content} img={item.picture}/>
-                    </TouchableOpacity>
-                )}
-            />
-        </>
+        <FlatList
+            style={{padding: 15}}
+            refreshing={<RefreshControl refreshing={isLoading} onRefresh={onFetchPosts}/>}
+            data={posts}
+            renderItem={({item}) => (
+                <TouchableOpacity onPress={() => navigation.navigate('post', {id: item.id, title: item.title})}>
+                    <Post title={item.title} body={item.content} img={item.picture}/>
+                </TouchableOpacity>)}
+        />
     );
 }
-
-const Posts = styled.ScrollView`
-  padding: 10px;
-`
--- screens/Post.jsx
+++ screens/Post.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import {Text} from "react-native";
+import {Image, Text, View} from "react-native";
+import ss from '../components/Post.module.scss'
+import axios from "axios";
 
 
-export const Post = ()  => {
+export const Post = ({route, navigation}) => {
+    const {id, title} = route.params
+    const [post, setPost] = React.useState(null)
+    React.useEffect(() => {
+        navigation.setOptions({title})
+        axios.get(`https://freefakeapi.io/api/posts/${id}`).then(({data}) => {
+            setPost(data)
+        })
+    })
+
     return (
-        <Text>Post</Text>
+        <View style={ss.full_post}>
+            <Image source={{uri: post?.picture}} style={ss.full_post_img}/>
+            <Text style={ss.title}>{post?.title}</Text>
+            <Text style={ss.full_post_text}>{post?.content}</Text>
+        </View>
     );
 }
